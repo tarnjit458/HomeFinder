a0f11c41311ff382c6608de8980c0486230983ba
tarnjit458 <tarnjitishere@gmail.com>
2020-10-31 20:48:53 -0700

    Added a Register component and added states for both the Login/Register components

--- home-finder/Frontend/src/App.js
+++ home-finder/Frontend/src/App.js
@@ -1,51 +1,60 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Login from './components/Login.jsx';
-import Home from './components/Home.jsx';
-import Buy from './components/Buy.jsx';
-import Rent from './components/Rent.jsx';
-import RentSell from './components/Rental-Sell.jsx';
-import Layout from './components/Layout.jsx';
-import Profile from './components/Profile.jsx';
 import ListingForm from './components/ListingForm.jsx';
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Home from "./components/Home.jsx";
+import Buy from "./components/Buy.jsx";
+import Rent from "./components/Rent.jsx";
+import RentSell from "./components/Rental-Sell.jsx";
+import Layout from "./components/Layout.jsx";
+import Profile from "./components/Profile.jsx";
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import PageNotFound from './components/PageNotFound';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <Layout>
       <Router>
         <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/buy">
-              <Buy />
-            </Route>
-            <Route path="/rent">
-              <Rent />
-            </Route>
-            <Route path="/rent-sell">
-              <RentSell />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-            <Route path="/list_form">
-              <ListingForm />
-            </Route>
-            <Route exact path="/">
-              <Redirect to="/home"/>
-            </Route>
-            
-            <Route path="*">
-              <PageNotFound />  
-            </Route> 
+          <Route path="/list_form">
+            <ListingForm />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/buy">
+            <Buy />
+          </Route>
+          <Route path="/rent">
+            <Rent />
+          </Route>
+          <Route path="/rent-sell">
+            <RentSell />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+
+          <Route path="*">
+            <PageNotFound />
+          </Route>
         </Switch>
       </Router>
     </Layout>
