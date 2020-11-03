72cd46de78571d2f3108a024dacb1bd2f0c4b666
huiyingli0125 <huiyingli2014@gmail.com>
2020-11-02 16:19:07 -0800

    add serializer file

---
+++ home-finder/Backend/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, House
+
+class UserSerializer(serializers.ModelSerilizer):
+	class Meta:
+		model = User
+		fields = ('id', 'first_name', 'last_name', 'address', 'phone', 'email', 'date_joined')
+
+class HouseSerializer(serializers.ModelSerilizer):
+	class Meta:
+		model = House
+		fields = ('id', 'address', 'city', 'state', 'zip_code', 'cost', 'description', 'for_sale', 'owner', 'registered_at', 'sold')
