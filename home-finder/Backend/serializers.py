a8383cdd578cc13c6aecc98e868906450be20e9c
huiyingli0125 <huiyingli2014@gmail.com>
2020-11-05 15:56:37 -0800

    update serializers.py

--- home-finder/Backend/serializers.py
+++ home-finder/Backend/serializers.py
@@ -4,9 +4,9 @@ from .models import User, House
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('id', 'first_name', 'last_name', 'address', 'phone', 'email', 'date_joined')
+		fields = ('id', 'first_name', 'last_name', 'address', 'city', 'state', 'zip_code', 'phone', 'email', 'date_joined')
 
 class HouseSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = House
-		fields = ('id', 'address', 'city', 'state', 'zip_code', 'cost', 'description', 'for_sale', 'owner', 'registered_at', 'sold')
+		fields = ('id', 'address', 'city', 'state', 'zip_code', 'cost', 'description', 'for_sale', 'owner', 'registered_at', 'sold', "on_loan")
