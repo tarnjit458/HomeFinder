5b3b11ad2465901bca3e38bf1388f6b07a309fda
tarnjit458 <tarnjitishere@gmail.com>
2020-11-20 16:11:59 -0800

    Created a basic search component and added it to the Buy and Rent menu

--- home-finder/Frontend/src/components/Rent.jsx
+++ home-finder/Frontend/src/components/Rent.jsx
@@ -1,13 +1,63 @@
-import React from 'react';
+import React from "react";
+import Search from "./Search.jsx";
+
+import {
+  Card,
+  CardBody,
+  Button,
+  CardTitle,
+  Row,
+  Col,
+  CardDeck,
+} from "reactstrap";
 
 class Rent extends React.Component {
-    render() {
-        return(
-            <div>
-                Find places to Rent.
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredHomes: [],
+    };
+  }
+  myCallback = (dataFromChild) => {
+    this.setState({ filteredHomes: dataFromChild });
+  };
+
+  render() {
+    const homesJsx = this.state.filteredHomes.map((home) => {
+      return (
+        <div key={home.id}>
+          <Col md="8">
+            <CardDeck style={{ width: "20rem" }}>
+              <Card>
+                <CardBody>
+                  <img
+                    className="card-img-top"
+                    src={`${home.image}`}
+                    alt="failed"
+                  />
+                  <CardTitle tag="h5">
+                    {`${home.address}, ` +
+                      `${home.city}, ` +
+                      `${home.state} ` +
+                      `${home.zip_code}`}
+                  </CardTitle>
+                  <Button onClick={this.manageHouseToggle} color="primary">
+                    Buy Home
+                  </Button>{" "}
+                </CardBody>
+              </Card>
+            </CardDeck>
+          </Col>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <Search callbackFromParent={this.myCallback} />
+        <Row>{homesJsx}</Row>
+      </div>
+    );
+  }
 }
 
 export default Rent;
---
+++ home-finder/Frontend/src/components/Search.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Container, Input } from "reactstrap";
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      homes: [],
+      filteredHomes: [],
+      currentInput: "",
+      filterOption: "all",
+      search: false,
+    };
+  }
+
+  async componentDidMount() {
+    const url = "http://127.0.0.1:8000/api/allHouses/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ homes: data });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  filterHomes = () => {
+    this.state.filteredHomes = this.state.homes.filter((home) => {
+      if (this.state.filterOption === "all") {
+        return home;
+      } else if (this.state.filterOption === "zip_code") {
+        if (
+          home.zip_code
+            .toLowerCase()
+            .startsWith(this.state.currentInput.toLowerCase())
+        ) {
+          return home;
+        }
+      } else if (this.state.filterOption === "address") {
+        if (
+          home.address
+            .toLowerCase()
+            .startsWith(this.state.currentInput.toLowerCase())
+        ) {
+          return home;
+        }
+      } else if (this.state.filterOption === "city") {
+        if (
+          home.city
+            .toLowerCase()
+            .startsWith(this.state.currentInput.toLowerCase())
+        ) {
+          return home;
+        }
+      }
+    });
+    this.setState({ search: true });
+    this.props.callbackFromParent(this.state.filteredHomes);
+  };
+
+  render() {
+    const justifySearch = {
+      flex: "none",
+      padding: "32px 21px",
+    };
+
+    return (
+      <Container>
+        <div className="row justify-content-end" style={justifySearch}>
+          <div className="col-5">
+            <Input
+              type="text"
+              name="currentInput"
+              placeholder="Enter an address, zip code or city..."
+              value={this.state.currentInput}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <Input
+              type="select"
+              name="filterOption"
+              value={this.state.filterOption}
+              onChange={this.handleChange}
+            >
+              <option value="all">Select a Filter</option>
+              <option value="address">Street Address</option>
+              <option value="zip_code">Zip Code</option>
+              <option value="city">City</option>
+            </Input>
+          </div>
+          <div className="col-4">
+            <button
+              className="btn btn-secondary my-2 my-sm-0"
+              type="submit"
+              onClick={this.filterHomes}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default Search;
