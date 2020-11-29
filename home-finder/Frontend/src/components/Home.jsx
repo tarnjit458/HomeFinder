e5da6001e848462ec7b711b5b4c88c99095a00b6
Eric Lin <eric.lin@sjsu.edu>
2020-11-28 16:17:26 -0800

    Realtor now can't access rent card in home page.

--- home-finder/Frontend/src/components/Home.jsx
+++ home-finder/Frontend/src/components/Home.jsx
@@ -1,69 +1,62 @@
-import React from 'react';
-import PropertyCard from './PropertyCard.jsx';
-import {Link} from 'react-router-dom';
+import React from "react";
+import PropertyCard from "./PropertyCard.jsx";
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentInput : ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentInput: "",
+      role: localStorage.getItem("role"),
+    };
+  }
 
-    searchHomes = (e) => {
-        // keep user from clicking submit button multiple times
-        // e.preventDefault();
-        // This method will call a url using axios to search for the specific address 
-        // or list of homes close to the address for sale
+  handleChange = (e) => {
+    this.setState({ currentInput: e.target.value });
+  };
 
-        console.log(`You clicked search!!! ${this.state.currentInput}`);
-    }
+  render() {
+    const justifyCards = {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      alignItems: "center",
+      flex: "none",
+      padding: "32px 21px",
+    };
 
-    handleChange = (e) => {
-        this.setState({currentInput: e.target.value});
-    }
+    return (
+      <div>
+        <div className="row justify-content-center">
+          <h2>We'll find your next home</h2>
+        </div>
 
-    render() {
-
-        const justifyCards = {
-            "flexDirection": "row",
-            "flexWrap": "wrap",
-            "alignItems": "center",
-            "flex": "none",
-            "padding": "32px 21px"
-        }
-
-        return(
-            <div>
-                <div className="row justify-content-center">
-                    <h2>
-                        We'll find your next home
-                    </h2>
-                </div>
-                
-                <div className="row card-deck" style={justifyCards}>
-                    <PropertyCard title="Buy a House!" 
-                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                    image_src="/960x0.jpg"
-                    button="Search Homes"
-                    button_src="/buy"
-                    />
-                    <PropertyCard title="Sell your house!" 
-                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                    image_src="/For-sale-sign.jpg"
-                    button="Make Money"
-                    button_src="/sell"
-                    />
-                    <PropertyCard title="Find a place to rent!" 
-                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                    image_src="/cover-12.jpg"
-                    button="Find Rentals"
-                    button_src="/rent"
-                    />
-                </div>
-            </div>
-        );
-    }
+        <div className="row card-deck" style={justifyCards}>
+          <PropertyCard
+            title="Buy a House!"
+            description="Some quick example text to build on the card title and make up the bulk of the card's content."
+            image_src="/960x0.jpg"
+            button="Search Homes"
+            button_src="/buy"
+          />
+          <PropertyCard
+            title="Sell your house!"
+            description="Some quick example text to build on the card title and make up the bulk of the card's content."
+            image_src="/For-sale-sign.jpg"
+            button="Make Money"
+            button_src="/sell"
+          />
+          {this.state.role !== "realtor" ? (
+            <PropertyCard
+              title="Find a place to rent!"
+              description="Some quick example text to build on the card title and make up the bulk of the card's content."
+              image_src="/cover-12.jpg"
+              button="Find Rentals"
+              button_src="/rent"
+            />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
